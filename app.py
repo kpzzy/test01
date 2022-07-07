a318b8458b266e0141197c18b67d68a472e8c106
Changsoon-YUN <92949174+Changsoon-Yun@users.noreply.github.com>
2022-07-07 19:28:11 +0900

    Add files via upload
    .

--- app.py
+++ app.py
@@ -1,72 +1,52 @@
+import requests
+
 from flask import Flask, render_template, request, jsonify
-import datetime
-
-app = Flask(__name__)
-
+from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.lpapihe.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://changsoon:tnsrh124!1@cluster0.ry8gyso.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
-import hashlib
-import jwt
-import bcrypt
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.daum.net/ranking/reservation', headers=headers)
 
-SECRET_KEY = 'FREE'
+soup = BeautifulSoup(data.text, 'html.parser')
+
+app = Flask(__name__)
+movies = soup.select('#mainContent > div > div.box_ranking > ol > li > div')
+
+for movie in movies:
+    a = movie.select_one('div.thumb_cont > strong > a')
+    if a is not None:
+        title = a.text
+        rank = movie.select_one('div > div.thumb_item > div.poster_movie > span.rank_num').text
+        grade = movie.select_one('div.thumb_cont > span.txt_append > span:nth-child(1) > span').text
+        rate = movie.select_one('div.thumb_cont > span.txt_append > span:nth-child(2) > span').text
+        date = movie.select_one('div.thumb_cont > span.txt_info > span').text
+        img = movie.select_one('div.thumb_item > div.poster_movie > img')['src']
+
+        doc = {
+            'rank': rank,
+            'title': title,
+            'rate': rate,
+            'date': date,
+            'img': img,
+            'grade': grade
+        }
 
 
+        db.movies.insert_one(doc)
 
-# 메인페이지
 @app.route('/')
 def home():
-    return render_template('repeat1.html')
-# 회원가입화면
-@app.route('/signup')
-def singup():
-    return render_template('signup.html')
-# 로그인화면
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-# 여기는 아이디와 pw저장 api
-@app.route("/api/signup", methods=["POST"])
-def api_signup():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
-
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+   return render_template('index.html')
 
 
-    doc = {'id': id_receive, 'pw': pw_hash}
-    db.login.insert_one(doc)
-
-    return jsonify({'msg': '저장완료'})
-
-# 저장된 정보로 로그인하기 이거 왜안돼 씨발
-@app.route('/api/login', methods=['POST'])
-def api_login():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
-
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-
-    result = db.login.find_one({'id':id_receive, 'pw':pw_hash})
-
-    if result is not None:
-        payload = {
-            'id' : id_receive,
-            'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
-        return jsonify({'msg' : '저장완료', 'token':token})
-    else:
-        return jsonify({'msg' : '틀림'})
-
-
-
+@app.route("/movies", methods=["GET"])
+def movies_get():
+    movie_list = list(db.movies.find({},{'_id':False}))
+    return jsonify({'orders': movie_list})
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
